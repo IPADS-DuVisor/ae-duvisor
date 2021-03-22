c9a016c5663f2b30ce4682866951d08828639dee
Zeyu Mi <yzmizeyu@gmail.com>
2021-03-22 10:11:40 +0800

    minor: add simple_test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ mod tests {
     fn unit_test_example() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn test_simple_add() {
+        use crate::simple_add;
+        assert_eq!(5, simple_add(2, 3));
+    }
+}
+
+pub fn simple_add(value1: i32, value2 :i32) -> i32 {
+    value1 + value2
 }
 
 fn main() {
