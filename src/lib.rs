c9b2dc43344ed94d0817a8e01b32b7c39585dae5
Zeyu Mi <yzmizeyu@gmail.com>
2021-03-23 19:40:08 +0800

    impl new for VMConfig

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,13 @@ pub fn simple_add(value1: i32, value2 :i32) -> i32 {
     value1 + value2
 }
 
-use std::error::Error;
-use std::path::Path;
+use std::fs;
 
-pub struct Config {
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+pub struct VMConfig {
     pub vcpu_count: u32,
     pub mem_size: u32,
     pub machine_type: String,
@@ -28,25 +31,96 @@ pub struct Config {
     pub dtb_path: String,
 }
 
+impl VMConfig {
+    pub fn new() -> Result<VMConfig, &'static str> {
+        let mut vm_config = VMConfig {
+            vcpu_count: 0,
+            mem_size: 0,
+            machine_type: String::from(""),
+            kernel_img_path: String::from(""),
+            initrd_path: String::from(""),
+            dtb_path: String::from(""),
+        };
+
+        let yaml = load_yaml!("clap_config.yml");
+        let matches = App::from_yaml(yaml).get_matches();
+
+        // We get VM arguments from vm_config file. 
+        if matches.is_present("vm_config") {
+            let vm_config_path = matches.value_of("vm_config").unwrap().to_string();
+            println!("vm_config_path = {}", vm_config_path);
+            let vm_config_contents = match fs::read_to_string(vm_config_path) {
+                Ok(contents) => contents,
+                Err(_e) => return Err("Failed to read vm_config file"),
+            };
+
+            if let Err(e) = parse_vm_config_file(&vm_config_contents,
+                                                         &vm_config) {
+                return Err(e);
+            }
+        } else { // We get VM arguments from command line.
+            // Get machine type 
+            if matches.is_present("machine") {
+                vm_config.machine_type = matches.value_of("machine").unwrap().to_string();
+            }
+
+            // Get vcpu count
+            vm_config.vcpu_count = value_t!(matches.value_of("smp"), u32).unwrap_or(0);
+            if vm_config.vcpu_count == 0 {
+                return Err("Error: please provide vcpu count by using --smp or config files.");
+            }
+
+            // Get memory size
+            vm_config.mem_size = value_t!(matches.value_of("memory"), u32).unwrap_or(0);
+            if vm_config.mem_size == 0 {
+                return Err("Error: please provide memory size by using --memory or config files.");
+            }
+
+            // Get kernel_image_path 
+            if matches.is_present("kernel") {
+                vm_config.kernel_img_path = matches.value_of("kernel").unwrap().to_string();
+            } else {
+                return Err("Error: please provide kernel image by using --kernel or config files.");
+            }
+
+            // Get dtb_path 
+            if matches.is_present("dtb") {
+                vm_config.dtb_path = matches.value_of("dtb").unwrap().to_string()
+            }
+
+            // Get initrd_path 
+            if matches.is_present("initrd") {
+                vm_config.initrd_path = matches.value_of("initrd").unwrap().to_string();
+            }
+        }
+
+        println!("machine_type = {}, smp = {}, mem_size = {}, kernel_img_path = {}, dtb_path = {}, initrd_path = {}",
+                 vm_config.machine_type,
+                 vm_config.vcpu_count, vm_config.mem_size,
+                 vm_config.kernel_img_path, vm_config.dtb_path, vm_config.initrd_path);
+
+        Ok(vm_config)
+    }
+}
+
 /*
  * Parsing vm configs from the config file
  * All existing arguments in vm_config struct will be overwritten.
  */
 pub fn parse_vm_config_file(contents: &String,
-                            vm_config: &Config) -> Result<(), &'static str>{
-
+                            vm_config: &VMConfig) -> Result<(), &'static str>{
     Ok(())
 }
 
 /*
  * Check whether arguments in vm_config is legal or not.
  */
-fn verify_args(vm_config: &Config) -> bool {
+fn verify_args(vm_config: &VMConfig) -> bool {
 
     true
 }
 
-pub fn run(config: Config) -> Result<(), &'static str> {
+pub fn run(config: &VMConfig) -> Result<(), &'static str> {
     if verify_args(&config) {
         return Err("Encounter illegal arguments");
     }
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,12 @@
 use std::process;
-use std::fs;
 use std::error::Error;
-use laputa::Config;
-
-#[macro_use]
-extern crate clap;
-use clap::App;
+use laputa::VMConfig;
 
 fn main() -> Result<(), Box<dyn Error>>{
-    let mut vm_config = Config {
-        vcpu_count: 0,
-        mem_size: 0,
-        machine_type: String::from(""),
-        kernel_img_path: String::from(""),
-        initrd_path: String::from(""),
-        dtb_path: String::from(""),
-    };
-
-    let yaml = load_yaml!("clap_config.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-
-
-    // We get VM arguments from vm_config file. 
-    if matches.is_present("vm_config") {
-        let vm_config_path = matches.value_of("vm_config").unwrap().to_string();
-        println!("vm_config_path = {}", vm_config_path);
-        let vm_config_contents = fs::read_to_string(vm_config_path)?;
-
-        if let Err(e) = laputa::parse_vm_config_file(&vm_config_contents,
-                                                      &vm_config) {
-            eprintln!("Failed to parse vm config file: {}", e);
-            process::exit(1);
-        }
-    } else { // We get VM arguments from command line.
-        // Get machine type 
-        if matches.is_present("machine") {
-            vm_config.machine_type = matches.value_of("machine").unwrap().to_string();
-        }
-
-        // Get vcpu count
-        vm_config.vcpu_count = value_t!(matches.value_of("smp"), u32).unwrap_or(0);
-        if vm_config.vcpu_count == 0 {
-            eprintln!("Error: please provide vcpu count by using --smp or config files.");
-            process::exit(1);
-        }
-
-        // Get memory size
-        vm_config.mem_size = value_t!(matches.value_of("memory"), u32).unwrap_or(0);
-        if vm_config.mem_size == 0 {
-            eprintln!("Error: please provide memory size by using --memory or config files.");
-            process::exit(1);
-        }
-
-        // Get kernel_image_path 
-        if matches.is_present("kernel") {
-            vm_config.kernel_img_path = matches.value_of("kernel").unwrap().to_string();
-        } else {
-            eprintln!("Error: please provide kernel image by using --kernel or config files.");
-            process::exit(1);
-        }
-
-        // Get dtb_path 
-        if matches.is_present("dtb") {
-            vm_config.dtb_path = matches.value_of("dtb").unwrap().to_string()
-        }
-
-        // Get initrd_path 
-        if matches.is_present("initrd") {
-            vm_config.initrd_path = matches.value_of("initrd").unwrap().to_string();
-        }
-    }
-
-    println!("machine_type = {}, smp = {}, mem_size = {}, kernel_img_path = {}, dtb_path = {}, initrd_path = {}",
-             vm_config.machine_type,
-             vm_config.vcpu_count, vm_config.mem_size,
-             vm_config.kernel_img_path, vm_config.dtb_path, vm_config.initrd_path);
-
+    let vm_config = VMConfig::new().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
 
     Ok(())
 }
