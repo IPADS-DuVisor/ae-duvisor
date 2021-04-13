5f82128800149a24373fd64475bd21d37d7dda84
Yuxuan Liu <1197744123@sjtu.edu.cn>
2021-04-13 18:59:23 +0800

    linux: make linux build scripts more reliable

--- scripts/local/build_linux.sh
+++ scripts/local/build_linux.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-cd linux-laputa && ./scripts-laputa/local/docker_build_linux.sh && cd ../
+cd linux-laputa
+./scripts-laputa/local/docker_build_linux.sh 
+if [ $? -ne 0 ]; then
+    exit -1
+fi
+cd ../
 rm -r mnt
 mkdir -p mnt
 sudo mount prepare/linux-kernel-vdisk.img mnt/
+echo "mnt directory mounted"
 sudo cp -r linux-laputa mnt/
 sudo umount mnt
+echo "mnt directory umounted"
