c00eb798a5e8a79a6c3938a3c30c02f76252fc46
Yuxuan Liu <1197744123@sjtu.edu.cn>
2021-04-14 15:55:34 +0800

    test: make test build faster

--- unitestfiles/test_images/build.sh
+++ unitestfiles/test_images/build.sh
@@ -9,28 +9,37 @@ main() {
 	BUILD_DIR=${BUILD_DIR_TMP:-build}
 	TARGET_DIR=$2
 	CLEAN_FLAG=$3
-	if [ $CLEAN_FLAG == clean ]; then
+	if [ ${CLEAN_FLAG}1 == clean1 ]; then
 		echo "clean all test images"
 		rm -rf $BUILD_DIR
 		rm ./$TARGET_DIR/*.img
 		exit 0
 	fi
-	rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
 
-	cd $BUILD_DIR
-	echo "compiling vm test images..."
+	if [ -d $BUILD_DIR ]; then
+		cd $BUILD_DIR
+		echo "compiling vm test images..."
+		ninja
+		echo "vm test images compile succeed!"
+		exit 0
+	else
+		mkdir -p $BUILD_DIR
 
-	cmake \
-		-DCMAKE_LINKER=riscv64-linux-gnu-ld \
-		-DCMAKE_C_LINK_EXECUTABLE="<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" \
-		-DCMAKE_ASM_LINK_EXECUTABLE="<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" \
-		${SCRIPT_DIR} -G Ninja ..
+		cd $BUILD_DIR
+		echo "compiling vm test images..."
 
-	ninja
-	cd -
-	echo "vm test images compile succeed!"
-	mv ./$BUILD_DIR/*.img ./$TARGET_DIR/
-	exit 0
+		cmake \
+			-DCMAKE_LINKER=riscv64-linux-gnu-ld \
+			-DCMAKE_C_LINK_EXECUTABLE="<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" \
+			-DCMAKE_ASM_LINK_EXECUTABLE="<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" \
+			${SCRIPT_DIR} -G Ninja ..
+
+		ninja
+		cd -
+		echo "vm test images compile succeed!"
+		mv ./$BUILD_DIR/*.img ./$TARGET_DIR/
+		exit 0
+	fi
 }
 
 main $@
