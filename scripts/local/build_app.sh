7b5d7b379392485f69d9208f31c6d1cb8cc50b8f
Yuxuan Liu <1197744123@sjtu.edu.cn>
2021-04-21 15:46:53 +0800

    scripts: put app/ to linux-laputa/tools/laputa-test

--- scripts/local/build_app.sh
+++ scripts/local/build_app.sh
@@ -1,8 +1,8 @@
-cd app 
+cd linux-laputa/tools/laputa-test
 make 
-cd ../
+cd -
 
 mkdir -p mnt
 sudo mount ./prepare/ubuntu-vdisk.img ./mnt
-sudo cp -r app ./mnt/laputa
+sudo cp linux-laputa/tools/laputa-test/ioctl_test ./mnt/laputa
 sudo umount ./mnt
