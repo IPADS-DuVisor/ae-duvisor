985c61f3b3bd72747fba34a5d5d4bf2e19d93340
chenjh <chenjhsys@163.com>
2021-04-28 16:27:10 +0800

    format tests/

--- tests/vcpu_tests.rs
+++ tests/vcpu_tests.rs
@@ -20,35 +20,16 @@ fn test_vcpu_add_all_gprs() {
     // TODO: VirtualMachine::new, there should be assert codes. When vm initialization failure,
     // such as memory allocation failure or exceeding vm number failure, occurs, 
     // VirtualMachine::new should assert ABORT, and the tests will failed.
-    //let mut vm = VirtualMachine::new(vm_config.vcpu_count);
 
     // TODO: use constants to specify gpr
     // TODO: laputa::set_one_gregs(vcpu_num: u64, gpr_num: u64, val: u64)
-    // set one gpr of vcpu_num using gpr_num
-    /* 
-     * for gpr_num in gpr_nums {
-     *     laputa::set_one_gregs(0, gpr_num, 77);
-     * }  
-     */
-
-
-    //vm.vm_run();
 
     // TODO: use constants to specify exit reason
     // TODO: laputa::get_exit_reason(vcpu_num: u64), get exit reason of vcpu with number vcpu_num
-    /*
-     * let exit_reason = laputa::get_exit_reason(0);
-     * assert_eq!(exit_reason, TEST_ECALL_EXIT);
-     */
-    
 
     // TODO: use constants to specify gpr
     // TODO: laputa::get_one_greg(vcpu_num: u64, gpr_num: u64)
-    // get one gpr of vcpu_num using gpr_num
-    /*
-     * let a0 = laputa::get_one_greg(0, a0);
-     * assert_eq!(a0, 77 * 31);
-     */
+
     assert_eq!(0, 0);
 }
 
