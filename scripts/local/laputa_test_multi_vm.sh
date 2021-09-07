2f820600a8cb58049c34b94719eee248eef726d3
chenjh <chenjhsys@163.com>
2021-09-07 21:52:12 +0800

    only run smp for CI

--- scripts/local/laputa_test_multi_vm.sh
+++ scripts/local/laputa_test_multi_vm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./scripts/local/multi_vcpu/laputa_test_multi_vm_1_2.sh
-./scripts/local/multi_vcpu/laputa_test_multi_vm_1_4.sh
+#./scripts/local/multi_vcpu/laputa_test_multi_vm_1_2.sh
+#./scripts/local/multi_vcpu/laputa_test_multi_vm_1_4.sh
 # ./scripts/local/multi_vcpu/laputa_test_multi_vm_1_8.sh
 ./scripts/local/multi_vcpu/laputa_test_multi_vm_2_2.sh
 ./scripts/local/multi_vcpu/laputa_test_multi_vm_2_4.sh
-# ./scripts/local/multi_vcpu/laputa_test_multi_vm_2_8.sh
+./scripts/local/multi_vcpu/laputa_test_multi_vm_2_8.sh
