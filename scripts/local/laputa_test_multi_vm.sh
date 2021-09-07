09e3cacea9b144cc551edd52407192baa0a9623b
chenjh <chenjhsys@163.com>
2021-09-07 09:37:58 +0800

    remove 8-vm test for time limitation

--- scripts/local/laputa_test_multi_vm.sh
+++ scripts/local/laputa_test_multi_vm.sh
@@ -2,7 +2,7 @@
 
 ./scripts/local/multi_vcpu/laputa_test_multi_vm_1_2.sh
 ./scripts/local/multi_vcpu/laputa_test_multi_vm_1_4.sh
-./scripts/local/multi_vcpu/laputa_test_multi_vm_1_8.sh
+# ./scripts/local/multi_vcpu/laputa_test_multi_vm_1_8.sh
 ./scripts/local/multi_vcpu/laputa_test_multi_vm_2_2.sh
 ./scripts/local/multi_vcpu/laputa_test_multi_vm_2_4.sh
-./scripts/local/multi_vcpu/laputa_test_multi_vm_2_8.sh
+# ./scripts/local/multi_vcpu/laputa_test_multi_vm_2_8.sh
