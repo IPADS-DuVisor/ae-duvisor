cef937c3ea84e98b1526b4260862b9d0acac7f8a
Yuxuan Liu <1197744123@sjtu.edu.cn>
2021-09-28 16:19:54 +0800

    update linux build scripts

--- scripts/local/build_linux.sh
+++ scripts/local/build_linux.sh
@@ -2,7 +2,12 @@
 cd linux-laputa
 sed -i "s/CONFIG_ULH_FPGA=y/CONFIG_ULH_QEMU=y/g" .config
 sed -i "s/# CONFIG_ULH_QEMU is not set/# CONFIG_ULH_FPGA is not set/g" .config
-./scripts-laputa/docker_build_linux.sh 
+
+export ARCH=riscv
+export CROSS_COMPILE=riscv64-linux-gnu-
+
+make -j16
+
 if [ $? -ne 0 ]; then
     exit -1
 fi
